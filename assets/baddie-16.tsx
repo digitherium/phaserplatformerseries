5eeee8e053d31dc3c40464c7a6f605533ac84e2d
digitherium <hello@digitherium.com>
2021-09-06 23:27:43 +0100

    added 24, tile map object layers with new sprites

---
+++ assets/baddie-16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="water" tilewidth="32" tileheight="32" tilecount="3" columns="3">
+ <image source="map/water.png" width="96" height="32"/>
+</tileset>
